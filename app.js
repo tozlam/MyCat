3fcc5caeaafc78d9ae0a62677c7d1573e72cb3c4
Administrator <631926331@qq.com>
2017-07-21 23:05:38 +0800

    1

---
+++ app.js
@@ -0,0 +1,11 @@
+var stage = new createjs.Stage("gameView");
+var gameView = new createjs.Container();
+stage.addChild(gameView);
+var s = new createjs.Shape();
+s.graphics.beginFill("#FF0000");
+s.graphics.drawCircle(50,50,25);
+s.graphics.endFill();
+gameView.addChild(s);
+
+createjs.Ticker.setFPS(30);
+createjs.Ticker.addEventListener("tick",stage)
